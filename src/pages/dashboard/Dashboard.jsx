fe4fe99b8b342889c591dcaef7310ff1d08ddcac
Johan Johnsson <johan@johnsson-net.se>
2025-02-22 13:14:45 +0100

    wip

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -3,9 +3,19 @@ import './Dashboard.css';
 const Dashboard = () => {
 
     return (
-        <>
-            <h1>Dashboard</h1>
-        </>
+        <div className='dashboard'>
+            <header className='db-header db-general'>
+                <p>Header</p>
+            </header>
+            <section className='db-section'>
+                <div className='db-sidebar db-general'>
+                    <p>Sidebar</p>
+                </div>
+                <div className='db-content db-general'>
+                    <p>Content</p>
+                </div>
+            </section>
+        </div>
     )
 }
 
