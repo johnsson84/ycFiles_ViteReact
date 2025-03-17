d7627ddaf04c48bb5b7131d55a91e04fe2ce8eba
Johan Johnsson <johan@johnsson-net.se>
2025-03-17 11:27:03 +0100

    formatting

--- src/pages/startpage/Startpage.jsx
+++ src/pages/startpage/Startpage.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Startpage.css'
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Startpage.css";
+import axios from "axios";
 
 const Startpage = () => {
   const [loginEnabled, setLoginEnabled] = useState(false);
@@ -10,20 +10,48 @@ const Startpage = () => {
 
   //#region LOGIN
   const loginButton = () => {
-    return (<button className='spform-button' type="button" onClick={() => setLoginEnabled(true)}>Login</button>)
-  }
+    return (
+      <button
+        className="spform-button"
+        type="button"
+        onClick={() => setLoginEnabled(true)}
+      >
+        Login
+      </button>
+    );
+  };
 
   const loginForm = () => {
     return (
-      <form className='spform'>
-        <label>Email:</label><br></br>
-        <input className='spform-input' type='text' name='email' value={loginFields.email} onChange={(e) => handleLoginFieldChange(e)} placeholder='enter your email...'></input><br></br>
-        <label>Password:</label><br></br>
-        <input className='spform-input' type='password' name='password' value={loginFields.password} onChange={(e) => handleLoginFieldChange(e)} placeholder='enter your password...'></input><br></br>
-        <button className='spform-button' onClick={(e) => login(e)}>Login</button>
+      <form className="spform">
+        <label>Email:</label>
+        <br></br>
+        <input
+          className="spform-input"
+          type="text"
+          name="email"
+          value={loginFields.email}
+          onChange={(e) => handleLoginFieldChange(e)}
+          placeholder="enter your email..."
+        ></input>
+        <br></br>
+        <label>Password:</label>
+        <br></br>
+        <input
+          className="spform-input"
+          type="password"
+          name="password"
+          value={loginFields.password}
+          onChange={(e) => handleLoginFieldChange(e)}
+          placeholder="enter your password..."
+        ></input>
+        <br></br>
+        <button className="spform-button" onClick={(e) => login(e)}>
+          Login
+        </button>
       </form>
-    )
-  }
+    );
+  };
 
   const [loginFields, setLoginFields] = useState({
     email: "",
@@ -31,58 +59,86 @@ const Startpage = () => {
   });
 
   const handleLoginFieldChange = (e) => {
-    setLoginFields((prev) => ({ ...prev, [e.target.name]: e.target.value}));
-  }
+    setLoginFields((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
   const login = async (e) => {
     e.preventDefault();
 
     try {
       const response = await axios.post(
-        `${import.meta.env.VITE_API_URL}/auth/login`, 
-        loginFields, 
+        `${import.meta.env.VITE_API_URL}/auth/login`,
+        loginFields,
         {
           withCredentials: true,
-        });
-      localStorage.setItem("user", response.data.username)
+        }
+      );
+      localStorage.setItem("user", response.data.username);
       console.log(response.data);
-      navigate('/dashboard');
+      navigate("/dashboard");
     } catch (err) {
       console.log("Catch: " + err);
     }
-  }
+  };
 
   useEffect(() => {
-  //  console.log(loginFields);
-  }, [loginFields])
+    //  console.log(loginFields);
+  }, [loginFields]);
 
   //#endregion
 
   //#region REGISTER
   const registerLink = () => {
-    return (<p className='sp-register-link'>Don't have an account? Register <a href="" onClick={(e) => showRegisterForm(e)}>here.</a></p>)
-  }
+    return (
+      <p className="sp-register-link">
+        Don't have an account? Register{" "}
+        <a href="" onClick={(e) => showRegisterForm(e)}>
+          here.
+        </a>
+      </p>
+    );
+  };
 
   const showRegisterForm = (e) => {
     e.preventDefault();
-    setRegisterEnabled(true)
-  }
+    setRegisterEnabled(true);
+  };
 
   const registerForm = () => {
     return (
-      <form className='spform'>
-        <label>Email:</label><br></br>
-        <input className='spform-input' type='text' name='email' value={registerFields.email} onChange={(e) => handleRegisterFieldChange(e)} placeholder='enter your email...'></input><br></br>
-        <label>Password:</label><br></br>
-        <input className='spform-input' type='password' name='password' value={registerFields.password} onChange={(e) => handleRegisterFieldChange(e)} placeholder='enter your password...'></input><br></br>
-        <button className='spform-button' onClick={(e) => register(e)}>Register</button>
+      <form className="spform">
+        <label>Email:</label>
+        <br></br>
+        <input
+          className="spform-input"
+          type="text"
+          name="email"
+          value={registerFields.email}
+          onChange={(e) => handleRegisterFieldChange(e)}
+          placeholder="enter your email..."
+        ></input>
+        <br></br>
+        <label>Password:</label>
+        <br></br>
+        <input
+          className="spform-input"
+          type="password"
+          name="password"
+          value={registerFields.password}
+          onChange={(e) => handleRegisterFieldChange(e)}
+          placeholder="enter your password..."
+        ></input>
+        <br></br>
+        <button className="spform-button" onClick={(e) => register(e)}>
+          Register
+        </button>
       </form>
-    )
-  }
+    );
+  };
 
   const handleRegisterFieldChange = (e) => {
-    setRegisterFields((prev) => ({ ...prev, [e.target.name]: e.target.value}));
-  }
+    setRegisterFields((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
   const [registerFields, setRegisterFields] = useState({
     email: "",
@@ -98,44 +154,42 @@ const Startpage = () => {
         registerFields,
         {
           withCredentials: true,
-        });
+        }
+      );
       console.log(response.data);
-      navigate('/');
+      navigate("/");
       window.location.reload();
     } catch (err) {
       console.log("Catch: " + err);
     }
-  }
+  };
 
   //#endregion
 
   //#region Page
-	return (
-		<div className="startpage">
-      <div className='sp-logo'>
+  return (
+    <div className="startpage">
+      <div className="sp-logo">
         <h1>ycFiles</h1>
         <p>your cloud files.</p>
       </div>
-      {!loginEnabled && !registerEnabled 
-      ? <>
-        {loginButton()}
-        {registerLink()}
-      </>
-       : null }
-      {loginEnabled && !registerEnabled
-      ? <>
-      {loginForm()}
-      {registerLink()}
-      </>
-      : null }
-      {registerEnabled
-      ? 
-      registerForm()
-      : null }
-      <p className='sp-build'>Built with: React + Vite</p>
-		</div>
-	)
-}
+      {!loginEnabled && !registerEnabled ? (
+        <>
+          {loginButton()}
+          {registerLink()}
+        </>
+      ) : null}
+      {loginEnabled && !registerEnabled ? (
+        <>
+          {loginForm()}
+          {registerLink()}
+        </>
+      ) : null}
+      {registerEnabled ? registerForm() : null}
+      <p className="sp-build">Built with: React + Vite</p>
+    </div>
+  );
+};
 //#endregion
 
 export default Startpage;
